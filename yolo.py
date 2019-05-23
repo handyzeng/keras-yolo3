4029a8d5452f34974373ffd5e0b7b2548e31a031
码夫祥子 <funzxj@hotmail.com>
2019-05-23 11:05:41 +0800

    add detect() function in YOLO
    detect() function used to only detect objects in image, but not draw the objects in the image. 
    Output the objects detected.

--- yolo.py
+++ yolo.py
@@ -99,6 +99,63 @@ class YOLO(object):
                 score_threshold=self.score, iou_threshold=self.iou)
         return boxes, scores, classes
 
+    def detect(self, image):
+        start = timer()
+
+        if self.model_image_size != (None, None):
+            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
+            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+        else:
+            new_image_size = (image.width - (image.width % 32),
+                              image.height - (image.height % 32))
+            boxed_image = letterbox_image(image, new_image_size)
+        image_data = np.array(boxed_image, dtype='float32')
+
+        #print(image_data.shape)
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        #print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+
+        #font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        #   size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+        #thickness = (image.size[0] + image.size[1]) // 300
+        
+        objs = []
+        for i, c in reversed(list(enumerate(out_classes))):
+            predicted_class = self.class_names[c]
+            box = out_boxes[i]
+            score = out_scores[i]
+
+            #label = '{} {:.2f}'.format(predicted_class, score)
+            #draw = ImageDraw.Draw(image)
+            #label_size = draw.textsize(label, font)
+
+            top, left, bottom, right = box
+            top = max(0, np.floor(top + 0.5).astype('int32'))
+            left = max(0, np.floor(left + 0.5).astype('int32'))
+            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+            #print(label, (left, top), (right, bottom))
+            obj = {'label': predicted_class,
+                    'bbox': { 'left': str(left), 'top': str(top), 'right': str(right), 'bottom': str(bottom) },
+                    'score': str(score)
+            }
+            objs.append(obj)
+
+        end = timer()
+        #print(end - start)
+        return objs
+    
     def detect_image(self, image):
         start = timer()
 
